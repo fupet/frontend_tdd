1d75cbeb7581680fadc3a7de9eb188173f281bfe
Fürst Péter <fupeti@gmail.com>
2014-10-06 22:17:28 +0200

    done

--- libs/Dropdown/view.js
+++ libs/Dropdown/view.js
@@ -1,5 +1,4 @@
 var Backbone = require('backbone'),
-
 	/**
 	 * @name module:Dropdown
 	 * @constructor
@@ -8,12 +7,38 @@ var Backbone = require('backbone'),
 	Dropdown = Backbone.Marionette.View.extend(
 		/** @lends module:Dropdown.prototype **/
 		{
-			ui : {
-
+			/**
+			 * The css classname, that indicates the open status of the submenu item.
+			 * @const {string}
+			 */
+			OPEN_CLASS : 'open',
+			ui         : {
+				/**
+				 * Menu item selector
+				 */
+				menuItems : 'a',
+				/**
+				 * Sub menu selector
+				 */
+				subMenu   : '+ ul'
 			},
 
 			events : {
+				'click @ui.menuItems' : 'onMenuItemClick'
+			},
 
+			/**
+			 * Handles the menu item click. Puts the open class if there is a submenu after the item.
+			 * @param {Jquery.Event} ev The jQuery event object.
+			 *
+			 * @return void
+			 */
+			onMenuItemClick : function(ev) {
+				var target = Backbone.$(ev.currentTarget);
+				// has submenu
+				if (target.find(this.ui.subMenu).length) {
+					target.toggleClass(this.OPEN_CLASS);
+				}
 			}
 		}
 	);
--- tests/Dropdown/view.js
+++ tests/Dropdown/view.js
@@ -1,14 +1,42 @@
 var assert = require('chai').assert,
 	sinon = require('sinon'),
 	Backbone = require('backbone'),
-
 	Dropdown = require('../../libs/Dropdown/view'),
 	testContent = require('./content/testcontent1.html');
 
 suite('testing dropdown view', function() {
 	setup(function() {
 		Backbone.$('#sandbox').html(testContent);
-
+		Dropdown.prototype.onMenuItemClick = sinon.spy(Dropdown.prototype, 'onMenuItemClick');
 		this.dropdown = new Dropdown({el : '#dropdown'});
+	});
+
+	teardown(function() {
+		Dropdown.prototype.onMenuItemClick.restore();
+	});
+
+	test('test menu click', function() {
+		this.dropdown.$(this.dropdown.ui.menuItems).eq(0).click();
+		assert.isTrue(this.dropdown.onMenuItemClick.calledOnce);
+	});
+
+	test('test is menu open', function() {
+		var testItem = this.dropdown.$(this.dropdown.ui.menuItems).eq(2);
+		testItem.click();
+		assert.isTrue(testItem.hasClass(this.dropdown.OPEN_CLASS));
+	});
+
+	test('test is menu close', function() {
+		var testItem = this.dropdown.$(this.dropdown.ui.menuItems).eq(0);
+		testItem.click().click();
+		assert.isTrue(this.dropdown.onMenuItemClick.calledTwice);
+		assert.isTrue(!testItem.hasClass(this.dropdown.OPEN_CLASS));
+	});
+
+	test('test subless menu item', function() {
+		var testItem = this.dropdown.$(this.dropdown.ui.menuItems).eq(1);
+		testItem.click();
+		assert.isTrue(this.dropdown.onMenuItemClick.calledOnce);
+		assert.isTrue(!testItem.hasClass(this.dropdown.OPEN_CLASS));
 	});
 });
